8979bbe8d103736c96514b97007f53a57edd95a9
ubahhukob@yandex.ru <ubahhukob@ya.ru>
2021-03-02 01:57:58 +0300

    first commit

---
+++ housework_1/src/main/java/spring/housework/App.java
@@ -0,0 +1,13 @@
+package spring.housework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
